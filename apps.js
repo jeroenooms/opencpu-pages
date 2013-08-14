a87a2fcbcffb30dd68cd0dd9db7e4f187dbdc5b2
Jeroen <jeroenooms@gmail.com>
2013-08-14 20:07:55 +0200

    update apps page

--- apps.js
+++ apps.js
@@ -4,21 +4,23 @@ $(function(){
     dataType: "jsonp"
   }).done(function(res){
     $.each(res.data, function(index, value) {
-      var coldiv = $('<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">').css("margin-left", "auto").css("margin-right", "auto")
+      var mydiv = $('<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">')
+         .css("text-align", "center")
          .appendTo("#appsrow");
-      var mydiv = $('<div>').width(200).css("margin-left", "auto").css("margin-right", "auto")
-         .appendTo(coldiv);
-      var myimg = $('<img class="img img-rounded">').width(100).height(100)
+      var myimg = $('<img class="appicon img img-rounded">').width(100).height(100)
          .attr("src", "https://raw.github.com/opencpu/" + value.name + "/master/inst/icon.png")
          .appendTo(mydiv).on("error", function(){
             myimg.attr("src", value.owner.avatar_url);
          });
-      var myul = $('<ul class="nav nav-tabs nav-stacked">')
+      $("<h3>").text(value.name).appendTo(mydiv);
+      var myul = $('<ul class="nav nav-stacked">')
+         .css("text-align", "left")
          .appendTo(mydiv);
-      $('<li>').text("Source code").appendTo(myul);
-      $('<li>').text("Package Info").appendTo(myul);
-      $('<li>').text("Live Demo").appendTo(myul);
-      $('<li><a href="#">Profile</a></li>').appendTo(myul)
+      
+      $('<li>').append($('<a>').attr("target", "_blank").attr("href", "http://www.github.com/opencpu/" + value.name).text("Source code")).appendTo(myul);
+      $('<li>').append($('<a>').attr("target", "_blank").attr("href", "/ocpu/github/opencpu/" + value.name).text("Package info")).appendTo(myul);
+      $('<li>').append($('<a>').attr("target", "_blank").attr("href", "/ocpu/github/opencpu/" + value.name + "/www").text("Live Demo")).appendTo(myul);
+
     });
   });
 });
