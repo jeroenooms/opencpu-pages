5ca710b9bd840dd1335e038147f91a5e474d1d2c
Jeroen Ooms <jeroenooms@gmail.com>
2017-05-13 19:37:40 +0200

    use cors instead of jsonp

--- apps.js
+++ apps.js
@@ -2,41 +2,37 @@ $(function(){
   $.ajax({
     url: "https://api.github.com/orgs/rwebapps/repos",
     dataType: "json"
-  }).done(function(data, status){
-    if(status == 200){
-      $.each(data, function(index, value) {
-        var mydiv = $('<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">')
-           .css("text-align", "center")
-           .appendTo("#appsrow");
-        var myimg = $('<img class="appicon img img-rounded">').width(100).height(100)
-           .attr("src", "https://raw.github.com/rwebapps/" + value.name + "/master/inst/icon.png")
-           .appendTo(mydiv).on("error", function(){
-              myimg.attr("src", value.owner.avatar_url);
-           });
+  }).done(function(data){
+    $.each(data, function(index, value) {
+      var mydiv = $('<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6">')
+         .css("text-align", "center")
+         .appendTo("#appsrow");
+      var myimg = $('<img class="appicon img img-rounded">').width(100).height(100)
+         .attr("src", "https://raw.github.com/rwebapps/" + value.name + "/master/inst/icon.png")
+         .appendTo(mydiv).on("error", function(){
+            myimg.attr("src", value.owner.avatar_url);
+         });
 
-        var mybr = $("<br />").appendTo(mydiv);
-        var btngrp = $('<div class="btn-group">').appendTo(mydiv);
-        var appbtn = $('<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">').text(value.name + " ").appendTo(btngrp);
-        var appurl;
+      var mybr = $("<br />").appendTo(mydiv);
+      var btngrp = $('<div class="btn-group">').appendTo(mydiv);
+      var appbtn = $('<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">').text(value.name + " ").appendTo(btngrp);
+      var appurl;
 
-        $.ajax({
-          url: "//rwebapps.ocpu.io/" + value.name
-        }).done(function(){
-          appurl = "https://rwebapps.ocpu.io/" + value.name;
-        }).error(function(){
-          appurl = "https://cran.ocpu.io/" + value.name;
-        }).always(function(){
-           var myul = $('<ul class="dropdown-menu" role="menu">').appendTo(btngrp);
-           myul.append('<li><a target="_blank" href="' + appurl + '/info"><i class="icon icon-info-sign"> Package Info </a></li>');
-           myul.append('<li><a target="_blank" href="' + appurl + '/www/"><i class="icon icon-play"> Live App Demo </a></li>');
-           myul.append('<li class="divider"></li>');
-           myul.append('<li><a target="_blank" href="http://www.github.com/rwebapps/' + value.name + '"><i class="icon icon-github"> Source Code </a></li>');
-           appbtn.append($('<span class="caret"></span>'))
-        });
+      $.ajax({
+        url: "//rwebapps.ocpu.io/" + value.name
+      }).done(function(){
+        appurl = "https://rwebapps.ocpu.io/" + value.name;
+      }).error(function(){
+        appurl = "https://cran.ocpu.io/" + value.name;
+      }).always(function(){
+         var myul = $('<ul class="dropdown-menu" role="menu">').appendTo(btngrp);
+         myul.append('<li><a target="_blank" href="' + appurl + '/info"><i class="icon icon-info-sign"> Package Info </a></li>');
+         myul.append('<li><a target="_blank" href="' + appurl + '/www/"><i class="icon icon-play"> Live App Demo </a></li>');
+         myul.append('<li class="divider"></li>');
+         myul.append('<li><a target="_blank" href="http://www.github.com/rwebapps/' + value.name + '"><i class="icon icon-github"> Source Code </a></li>');
+         appbtn.append($('<span class="caret"></span>'))
       });
-    } else {
-      alert(data.message || "http github api error " + status)
-    }
+    });
   }).fail(function(){
     $("#appsrow").append('<div class="alert alert-danger"> <a href="#" class="close" data-dismiss="alert">&times;</a><strong>Github API Error</strong> Could not load apps list from repository. Might be a problem with Github. Have a look at <a href="https://status.github.com/">status.github.com</a> and try again later.</div>');
   });
